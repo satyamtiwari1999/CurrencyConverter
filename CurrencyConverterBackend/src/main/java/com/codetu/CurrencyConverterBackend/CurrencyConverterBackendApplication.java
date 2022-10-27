f033ea0b6a40d5d5db4bff4e5e63f8cccefbf4a8
codetu <satymmtiwarii@gmail.com>
2022-10-27 18:11:31 +0530

    Initial Commit after backend boilerplate code setup

---
+++ CurrencyConverterBackend/src/main/java/com/codetu/CurrencyConverterBackend/CurrencyConverterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codetu.CurrencyConverterBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConverterBackendApplication.class, args);
+	}
+
+}
---
+++ CurrencyConverterBackend/src/test/java/com/codetu/CurrencyConverterBackend/CurrencyConverterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codetu.CurrencyConverterBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
